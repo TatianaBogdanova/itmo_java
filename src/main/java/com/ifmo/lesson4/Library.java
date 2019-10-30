20730f921c98afc4261ced7158338e7e386cea3b
Bogdanova Tatiana <bogdanova.t.yurye@sberank>
2019-10-30 22:18:09 +0300

    library

--- src/main/java/com/ifmo/lesson4/Library.java
+++ src/main/java/com/ifmo/lesson4/Library.java
@@ -86,7 +86,8 @@ public class Library {
         for (int count =0; count < kind.length; count++ )
         {
             Kind kindTemp = this.kind[count];
-            if ((kindTemp.book.author.equals(book.author)) && (kindTemp.book.title.equals(book.title))){
+            if ((kindTemp != null)&& (kindTemp.book.author.equals(book.author)) && (kindTemp.book.title.equals(book.title))){
+
                 if (kindTemp.quantity>quantity){
                     kindTemp.quantity-=quantity;
                     return quantity;
@@ -104,25 +105,5 @@ public class Library {
         return 0;
     }
 
-    public static void main(String[] args) {
-       Library library = new Library(2);
-      library.put(new Book("Stephen King", "Shining"), 2); // return true
-      library.put(new Book("Stephen King", "Dark Tower"), 3); // return true
 
-      // Эту книгу добавить не можем, т.к. лимит 2
-      library.put(new Book("Tolstoy", "War and peace"), 6); // return false
-
-      // Забираем все книги Тёмной башни, чтобы освободить место.
-      library.take(new Book("Stephen King", "Dark Tower"), 3); // return 3
-
-      // Теперь мы можем успешно добавить "Войну и мир".
-      library.put(new Book("Tolstoy", "War and peace"), 6); // return true
-
-
-     /*Library library = new Library(2);
-      library.put(new Book("Stephen King", "Shining"), 2); // return true
-
-      // Все равно вернет 2, т.к. больше нет.
-      System.out.println(library.take(new Book("Stephen King", "Shining"), 10)); // return 2
-    */}
 }
