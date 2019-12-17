53b240eabdafd4f4b4654e23d90107b34cfec9f3
Bogdanova Tatiana <bogdanova.t.yurye@sberank>
2019-12-17 20:40:37 +0300

    time

--- src/main/java/com/ifmo/lesson25/TimeToNewYear.java
+++ src/main/java/com/ifmo/lesson25/TimeToNewYear.java
@@ -1,23 +1,25 @@
 package com.ifmo.lesson25;
 
-import java.time.DayOfWeek;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 
 public class TimeToNewYear {
-    public static void getTimeToNy(){
-        LocalDate now = LocalDate.now();
-        LocalDate ny = LocalDate.parse("2020-01-01");
-        Period period = Period.between(now, ny);
-        System.out.println(period.getMonths() + " " + period.getDays() );
+    public static void getTimeToNy() {
+
+        Duration between = Duration.between(Instant.now(), Instant.parse("2020-01-01T00:00:00.00Z"));
+        System.out.println("days" + between.toDays() + " "
+                + " hours"+ (between.toHours()  - between.toDays()*24)  + " "
+                + " minut"+ (between.toMinutes() - (between.toHours()  - between.toDays()*24) *60 - between.toDays() *24*60));
     }
 
+    public static void myLongLife() {
 
-
+        Duration between = Duration.between(Instant.now(), Instant.parse("1988-04-05T17:00:00.00Z"));
+        System.out.println("sec" + between.toMinutes()*60);
+    }
 
     public static void main(String[] args) throws InterruptedException {
         getTimeToNy();
+        myLongLife();
     }
 }
